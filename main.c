5a0ccc80e4122c8dceadd9dd4080b93b6f0d4d98
Miyamoto <scotsdowne_road@yahoo.co.jp>
2016-09-02 10:13:49 +0900

    add comment

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "delay.h"
 #include "config.h"
-#include "stdlib.h"
 #include "xc.h"
 
 int main(void)
 {
     config_init();
     
-    TRISAbits.TRISA3 = 0;   //init digital port.
+    TRISAbits.TRISA3 = 0;   //init digital port connected to LED
     while(1){
-        LATAbits.LATA3 = rand()%2;
+        LATAbits.LATA3 = rand()%2;  //turn on or off LED
         delay_us(100);
     }
 }
